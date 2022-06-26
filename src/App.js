91d6f58dac6067b30dc3d67f3669142b533251fd
Drakodan <traxxdata@laposte.net>
2022-06-26 15:34:03 +0200

    Add rooter and empty CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Search from './components/Search';
+import Random from './components/Random';
+import Contact from './components/Contact';
+  
+//import './style.css';
+  
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>            
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/search">Recherche d'une recette</Link>
+            </li>
+            <li>
+              <Link to="/random">Une recette au hasard</Link>
+            </li>
+            <li>
+            <Link to="/contact">Une suggestion ?</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/random" element={<Random />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/components/Random.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Random() {
+  return <h2>Au hasard</h2>;
+}
+
+export default Random;
